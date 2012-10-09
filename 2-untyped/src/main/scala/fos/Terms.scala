2a2752bb2316a42c8c01ad13eeb25abfdf688881
Hao <julien19890118@gmail.com>
2012-10-09 13:47:44 +0200

    fix toString issue

--- 2-untyped/src/main/scala/fos/Terms.scala
+++ 2-untyped/src/main/scala/fos/Terms.scala
@@ -5,7 +5,7 @@ import scala.util.parsing.input.Positional
 /** Abstract Syntax Trees for terms. */
 abstract class Term extends Positional
 
-case class Var(x: String) extends Term{
+case class Var(x: String) extends Term {
   val v = x
   override def toString = x
 }
@@ -14,6 +14,11 @@ case class Abs(x: Var, t: Term) extends Term {
   override def toString = "\\" + x + "." + t
 }
 
-case class App(left: Term, right: Term) extends Term{
-  override def toString = "(" + left + ")" + " " + "(" + right + ")"
+case class App(left: Term, right: Term) extends Term {
+  override def toString = (left, right) match {
+    case (x: Var, y: Var) => left + " " + right
+    case (x: Var, y: Term) => left + " " + "(" + right + ")"
+    case (x: Term, y: Var) => "(" + left + ")" + " " + right
+    case (x: Term, y: Term) => "(" + left + ")" + " " + "(" + right + ")"
+  }
 }
