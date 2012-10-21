31a17069e0cc6b4de336ddf22ea949af45c6cc9b
Hao <julien19890118@gmail.com>
2012-10-21 23:01:06 +0200

    no Hashmap version

--- 3-typed/src/main/scala/fos/Terms.scala
+++ 3-typed/src/main/scala/fos/Terms.scala
@@ -23,9 +23,9 @@ case class Succ(t: Term) extends Term
 
 case class Pred(t: Term) extends Term 
 
-//case class Numeric(t: Int) extends Term {
-//  override def toString =  t.toString
-//}
+case class Numeric(t: Int) extends Term {
+  override def toString =  t.toString
+}
 
 case class If(t1: Term, t2: Term, t3: Term) extends Term 
 
