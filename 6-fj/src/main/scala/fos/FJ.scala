4f20b07a1b71d45d34927783ac5cfd52f10f710b
redocpot <julien19890118@gmail.com>
2012-12-23 15:37:33 +0100

    modify eval display

--- 6-fj/src/main/scala/fos/FJ.scala
+++ 6-fj/src/main/scala/fos/FJ.scala
@@ -172,8 +172,10 @@ object FJ extends StandardTokenParsers {
         try {
           cls foreach (cl => typeOf(cl, Nil))
           val typeExpr = typeOf(expr,Nil)
-          println("TYPE EXPR: "+typeExpr);//expr
-          reduceToValue(expr)
+          println("TYPE EXPR: "+typeExpr);
+          val exp = reduceToValue(expr)
+          println("EVALUATE TO: " + exp)
+          exp
         } catch {
           case TypeError(msg) =>
             println("Type Error: " + msg)
@@ -231,7 +233,7 @@ object FJ extends StandardTokenParsers {
     phrase(Prog)(tokens) match {
       case Success(trees, _) =>
         try {
-          print("EVALUATE TO: " + eval(trees))
+          eval(trees)
         } catch {
           case tperror => println(tperror.toString)
         }
